1327da6c76a672126a031960a28b17c842d72208
99002781 <72495949+99002781@users.noreply.github.com>
2020-10-08 15:53:31 +0530

    Create function.c

---
+++ function.c
@@ -0,0 +1,12 @@
+#include<function.h>
+#include<stdio.h>
+
+long factorial(int n)
+{
+   int c;
+   long result = 1;
+ 
+   for( c = 1 ; c <= n ; c++ )
+    result = result*c;
+   return ( result );
+}
